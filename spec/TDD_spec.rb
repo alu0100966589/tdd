62f2e7260207af3520df3fa9b85650dc6a67e4eb
Carlos Domínguez García <alu0100966589@ull.edu.es>
2017-10-22 19:46:25 +0100

    Food instances have a name property

--- spec/TDD_spec.rb
+++ spec/TDD_spec.rb
@@ -5,9 +5,15 @@ RSpec.describe TDD do
     expect(TDD::VERSION).not_to be nil
   end
 
-  it "There must be a Food class" do
+  it "Has a class Food" do
       # Expect "Food" to be an object of class "Class"
       expect( Food.is_a?(Class) ).to be(true)
+  end
+
+  it "Food instances have a name property and we can get the value" do
+      food_name = 'chocolate'
+      some_food = Food.new(food_name)
+      expect( some_food.name == food_name ).to be(true)
   end
 
 end
