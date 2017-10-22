c91bc800e49d523945e458fee94d2a1acadf5cb8
Carlos Domínguez García <alu0100966589@ull.edu.es>
2017-10-22 23:07:55 +0100

    Added array of nutrients quantities to Food instances

--- lib/TDD/Food.rb
+++ lib/TDD/Food.rb
@@ -5,9 +5,14 @@ class Food
         @@nutrients_name
     end
 
-    attr_reader :name
 
-    def initialize( name )
+    attr_reader :name, :nutrient_quantities
+
+    def initialize( name, nutrient_quantities )
+        raise( ArgumentError, "Invalid number of nutrients values" ) unless (nutrient_quantities.length == @@nutrients_name.length)
         @name = name
+        @nutrient_quantities = nutrient_quantities
     end
+
+
 end
--- spec/TDD_spec.rb
+++ spec/TDD_spec.rb
@@ -11,19 +11,31 @@ RSpec.describe TDD do
   end
 
   before :all do
+      @expected_nutrients_names = ["proteins", "carbohydrates", "fats"]
+      @food_nutrient_quantities = [1.2, 1.3, 1.4]
       @food_name = 'chocolate'
-      @some_food = Food.new(@food_name)
+      @some_food = Food.new(@food_name, @food_nutrient_quantities)
   end
 
   it "Food instances have a name property and we can get the value" do
       expect( @some_food.name == @food_name ).to be(true)
   end
 
-  it "Food has a static member array for storing the nutrient names" do
-      expected_nutrients_name = ["proteins", "carbohydrates", "fats"]
-      for i in (0...expected_nutrients_name.length)
-          expect( Food.nutrients_name[i] == expected_nutrients_name[i] ).to be(true)
-      end
+  it "Food has a static member array for storing the nutrients names" do
+      expect( Food.nutrients_name == @expected_nutrients_names ).to be(true)
+  end
+
+  it "Food instances have an array of nutrients" do
+      expect( @some_food.nutrient_quantities == @food_nutrient_quantities ).to be(true)
+  end
+
+  it "Array of nutrients have the same number of elements than the array of nutrients names" do
+      expect( @some_food.nutrient_quantities.length == @expected_nutrients_names.length ).to be(true)
+  end
+
+  it "An exception has to be thrown if the number of nutrients is not the same as expected" do
+      expect{ Food.new(@food_name, []) }.to raise_error(ArgumentError)
+      expect{ Food.new(@food_name, @food_nutrient_quantities * 2) }.to raise_error(ArgumentError)
   end
 
 end
