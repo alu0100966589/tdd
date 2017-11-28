08980e61b86ce89b91b7f97c417fea369c5c0929
Carlos Dominguez Garcia <alu0100966589@ull.edu.es>
2017-11-28 21:53:50 +0000

    Added info needed for coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,9 @@
 require "bundler/setup"
 require "TDD"
 require "benchmark"
+require "coveralls"
+
+Coveralls.wear!
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
